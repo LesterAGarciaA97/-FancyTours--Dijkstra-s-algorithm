322fbfcf3f16dc879374e904f5c6cc2a75b096df
Lester Andrés García Aquino <lestergarcia2015url@gmail.com>
2019-09-08 16:18:45 -0600

    Fancy Tours!

---
+++ MDII2017PR2/FancyTours/src/com/discreta/logica/ListaNodo.java
@@ -0,0 +1,41 @@
+package com.discreta.logica;
+
+import java.util.ArrayList;
+import com.discreta.modelo.Nodo;
+
+public class ListaNodo extends ArrayList<Nodo>{
+
+    public ListaNodo(){
+        super();
+    }
+    public Nodo buscarMenor(){
+        Nodo aux = new Nodo();
+        aux.setLongitudCamino(9999999);
+
+        for(Nodo nodo:this){
+            if(nodo.getLongitudCamino() < aux.getLongitudCamino()){
+                aux = nodo;                
+            }
+        }
+        return aux;
+    }
+    
+    public boolean isContenido(Nodo nodo){
+        boolean retornado = false;
+        for(Nodo n:this){
+            if(n == nodo){
+                retornado = true;
+            }
+        }
+        return retornado;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(Nodo n:this){
+            sb.append(n.getCirculo().getEtiqueta());
+        }
+        return sb.toString();
+    }
+}
